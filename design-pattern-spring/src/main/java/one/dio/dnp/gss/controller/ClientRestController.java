53476d3725f83d18b09e1eeeafb9328d122f5956
Gabr1el94 <bielsystem64@gmail.com>
2024-04-14 15:54:15 -0300

    add crud client and address

--- design-pattern-spring/src/main/java/one/dio/dnp/gss/controller/ClientRestController.java
+++ design-pattern-spring/src/main/java/one/dio/dnp/gss/controller/ClientRestController.java
@@ -15,9 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
-
-
 @RestController
 @RequestMapping("clients")
 public class ClientRestController {
--- design-pattern-spring/src/main/java/one/dio/dnp/gss/service/impl/ClientServiceImpl.java
+++ design-pattern-spring/src/main/java/one/dio/dnp/gss/service/impl/ClientServiceImpl.java
@@ -1,38 +1,70 @@
 package one.dio.dnp.gss.service.impl;
 
-import one.dio.dnp.gss.model.Client;
-import one.dio.dnp.gss.service.ClientService;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import one.dio.dnp.gss.model.Client;
+import one.dio.dnp.gss.model.Endereco;
+import one.dio.dnp.gss.model.respository.ClientRepository;
+import one.dio.dnp.gss.model.respository.EnderecoRepository;
+import one.dio.dnp.gss.service.ClientService;
+import one.dio.dnp.gss.service.ViaCepService;
+
+@Service
 public class ClientServiceImpl implements ClientService{
+
+    //Singleton
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private ViaCepService viaCepService;
 
     @Override
     public Iterable<Client> findClients() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findClients'");
+     return clientRepository.findAll();    
     }
 
     @Override
     public Client findClientbyId(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findClientbyId'");
+     Optional<Client> client = clientRepository.findById(id);
+     return client.get();    
     }
 
+    @SuppressWarnings("unused")
     @Override
     public void insertClient(Client client) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insertClient'");
+        saveClientWithCep(client);
     }
 
     @Override
     public void updateClient(Long id, Client client) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateClient'");
+        Optional<Client> clientBd = clientRepository.findById(id);
+        if (clientBd.isPresent()) {
+            saveClientWithCep(client);
+        }
     }
 
     @Override
     public void deleteClient(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteClient'");
+        clientRepository.deleteById(id);
+    }
+
+    //method save ou update client
+    private void saveClientWithCep(Client client) {
+        String cep = client.getAddress().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+            Endereco end = viaCepService.findCep(cep);
+            enderecoRepository.save(end);
+            return end;
+        });
+        client.setAddress(endereco);
+        clientRepository.save(client);
     }
     
 }
