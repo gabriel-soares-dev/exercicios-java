554d20c043d09628fe04fe2e3e1c5edb2f78967a
Gabr1el94 <bielsystem64@gmail.com>
2024-04-21 19:12:31 -0300

    init project API REST for Railway

---
+++ project-api-railway/src/main/java/me/dio/gss/projectapirailway/ProjectApiRailwayApplication.java
@@ -0,0 +1,13 @@
+package me.dio.gss.projectapirailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApiRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApiRailwayApplication.class, args);
+	}
+
+}
