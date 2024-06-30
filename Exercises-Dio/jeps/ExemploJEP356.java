8f74850811d70fdb4a3e0efcff48806a9e4fa5b5
Gabr1el94 <bielsystem64@gmail.com>
2024-06-29 23:38:00 -0300

    update ExemploJEP356 compare Random and SplittableRandom

--- Exercises-Dio/jeps/ExemploJEP356.java
+++ Exercises-Dio/jeps/ExemploJEP356.java
@@ -1,12 +1,33 @@
 package jeps;
-import java.util.random.*;
+
+import java.util.Random;
+import java.util.SplittableRandom;
+import java.util.stream.IntStream;
 
 public class ExemploJEP356 {
     public static void main(String[] args) {
-        RandomGeneratorFactory<RandomGenerator> factory = RandomGeneratorFactory.of("L64X128MixRandom");
-        RandomGenerator randomGenerator = factory.create(1994);
-        int randomNumber = randomGenerator.nextInt();
+        
+         int numIterations = 1_000_000;
 
-        System.out.println(randomNumber);
+        // Usando Random
+        long startTimeRandom = System.nanoTime();
+        Random random = new Random();
+        IntStream.range(0, numIterations)
+                .map(i -> random.nextInt(10))
+                .forEach(number -> {}); // Não fazemos nada com os números
+        long endTimeRandom = System.nanoTime();
+        long durationRandom = endTimeRandom - startTimeRandom;
+
+        // Usando SplittableRandom
+        long startTimeSplittableRandom = System.nanoTime();
+        SplittableRandom splittableRandom = new SplittableRandom();
+        IntStream.range(0, numIterations)
+                .map(i -> splittableRandom.nextInt(10))
+                .forEach(number -> {}); // Não fazemos nada com os números
+        long endTimeSplittableRandom = System.nanoTime();
+        long durationSplittableRandom = endTimeSplittableRandom - startTimeSplittableRandom;
+
+        System.out.println("Tempo de execução (Random): " + durationRandom + " ns");
+        System.out.println("Tempo de execução (SplittableRandom): " + durationSplittableRandom + " ns");
     }
 }
