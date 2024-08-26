907ac47b13a2b068bf200f338a3e6b82cca647ad
Gabr1el94 <bielsystem64@gmail.com>
2024-08-26 18:01:47 -0300

    function BinaryOperator for lambda

---
+++ Exercises-Dio/lambda/BinaryOperatorExample.java
@@ -0,0 +1,12 @@
+package lambda;
+import java.util.function.BinaryOperator;
+
+public class BinaryOperatorExample {
+    public static void main(String[] args) {
+        BinaryOperator<Double> calc = (x, y) -> { return x + y; }; 
+        System.out.println(calc.apply(2.0, 3.0));
+
+        calc = (x, y) -> { return x * y; }; 
+        System.out.println(calc.apply(2.0, 3.0));
+    }
+}
