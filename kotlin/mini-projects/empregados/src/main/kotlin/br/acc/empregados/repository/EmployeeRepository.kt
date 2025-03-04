057b21ce38fa9bda57124c88ed52b775d384e7a7
gabriel-soares-dev <bielsystem64@gmail.com>
2025-03-03 23:19:46 -0300

    remove project is not exists on kotlin

--- kotlin/mini-projects/empregados/src/main/kotlin/br/acc/empregados/repository/EmployeeRepository.kt
+++
@@ -1,7 +0,0 @@
-package br.acc.empregados.repository
-
-import br.acc.empregados.model.Employee
-import org.springframework.data.repository.CrudRepository
-
-interface EmployeeRepository: CrudRepository<Employee, Int> {
-}
