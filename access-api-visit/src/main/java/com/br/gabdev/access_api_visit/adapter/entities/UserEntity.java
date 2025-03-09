08d22fc5890010f0d0bdc8419db15ab42968d5d1
gabriel-soares-dev <bielsystem64@gmail.com>
2025-03-08 23:09:36 -0300

    missing add Data on entity user

--- access-api-visit/src/main/java/com/br/gabdev/access_api_visit/adapter/entities/UserEntity.java
+++ access-api-visit/src/main/java/com/br/gabdev/access_api_visit/adapter/entities/UserEntity.java
@@ -4,11 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class UserEntity {
 
 	@Id
